0b05c6270601153af02343fcb6f80e8f07e46807
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-12-24 14:40:39 +0200

    more logos

--- components/logo-marquee.tsx
+++ components/logo-marquee.tsx
@@ -14,7 +14,7 @@ export function LogoMarquee() {
 
   return (
     <div className="overflow-hidden">
-      <div className="relative overflow-hidden bg-black py-16 -rotate-[5deg] mt-32 mb-16 min-w-[120vw] -mx-[10vw] left-0">
+      <div className="relative overflow-hidden bg-white py-16 -rotate-[5deg] mt-32 mb-16 min-w-[120vw] -mx-[10vw] left-0 border-y-4 border-black">
         <div className="flex items-center gap-16 animate-marquee whitespace-nowrap">
           {[...items, ...items, ...items, ...items].map((item, index) => (
             <img key={index} src={item.logo || "/placeholder.svg"} alt={item.alt} className="h-12 w-auto" />
